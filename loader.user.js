66421cae5453b984532cb6b93939beb268256e63
mjrbordo <62356893+mjrbordo@users.noreply.github.com>
2026-04-05 14:56:51 +0200

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LOADER — Plemiona Scripts
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @updateURL    https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/loader.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/loader.user.js
 // @description  Ładuje wszystkie skrypty z mjrbordo/tamper
@@ -10,6 +10,7 @@
 // @require      https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/wasilewski.user.js
 // @require      https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/kolorowanie_zagrody.user.js
 // @require      https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/rozpiska_rozsyłanie.user.js
+// @require      https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/farma_auto.user.js
 // ==/UserScript==
 
 // Skrypt celowo pusty — całą robotę robią @require powyżej
