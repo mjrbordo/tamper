3ad70cc56f8860509020d3a580d260d50526cfd3
mjrbordo <62356893+mjrbordo@users.noreply.github.com>
2026-04-05 14:50:29 +0200

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -2,6 +2,8 @@
 // @name         LOADER — Plemiona Scripts
 // @namespace    http://tampermonkey.net/
 // @version      1.1
+// @updateURL    https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/loader.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/loader.user.js
 // @description  Ładuje wszystkie skrypty z mjrbordo/tamper
 // @author       mjrbordo
 // @match        https://*.plemiona.pl/*
