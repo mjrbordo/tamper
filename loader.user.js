1e09ee4690b844e4dbd8fd86034a9c2d3b7ee092
mjrbordo <62356893+mjrbordo@users.noreply.github.com>
2026-04-05 14:43:29 +0200

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -6,7 +6,7 @@
 // @author       mjrbordo
 // @match        https://*.plemiona.pl/*
 // @require      https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/wasilewski.user.js
-// @require      https://cdn.jsdelivr.net/gh/mjrbordo/tamper@mainkolorowanie_zagrody.user.js
+// @require      https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/kolorowanie_zagrody.user.js
 // ==/UserScript==
 
 // Skrypt celowo pusty — całą robotę robią @require powyżej
