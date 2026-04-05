28a3d15f6cc43da543298647016627b7eee5ecdd
mjrbordo <62356893+mjrbordo@users.noreply.github.com>
2026-04-05 14:40:03 +0200

    Create loader.user.js

---
+++ loader.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         LOADER — Plemiona Scripts
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Ładuje wszystkie skrypty z mjrbordo/tamper
+// @author       mjrbordo
+// @match        https://*.plemiona.pl/*
+// @require      https://cdn.jsdelivr.net/gh/mjrbordo/tamper@main/wasilewski.user.js
+// ==/UserScript==
+
+// Skrypt celowo pusty — całą robotę robią @require powyżej
